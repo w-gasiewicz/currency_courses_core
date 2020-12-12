9d612c23bdb411bf09382d91fb90617fe6721286
Wojciech Gąsiewicz <235086@student.pwr.edu.pl>
2020-12-13 00:45:49 +0100

    creating charts

--- currency_courses_core/ClientApp/src/components/Charts.js
+++ currency_courses_core/ClientApp/src/components/Charts.js
@@ -1,12 +1,16 @@
 ﻿import React, { Component } from 'react';
 import ChartsMenu from './ChartsMenu.js';
+import Chart from './Chart.js';
 
 export class Charts extends Component {
     static displayName = Charts.name;
 
     render() {
         return (
-            <ChartsMenu/>
+            <div>
+                <ChartsMenu />
+                <Chart />
+            </div>
         );
     }
 }
